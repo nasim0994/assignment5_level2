b3edd6032b35278c601d07d48c5606eb78d0082c
nasim0994 <nasimuddin0994@gmail.com>
2025-02-15 19:24:05 +0600

    add footer

--- frontend/src/app/(pages)/(mainLayout)/layout.tsx
+++ frontend/src/app/(pages)/(mainLayout)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/shared/main/Footer";
 import Header from "@/components/shared/main/Header";
 
 type props = { children: React.ReactNode };
@@ -7,7 +8,7 @@ export default function MainLayout({ children }: props) {
     <div>
       <Header />
       <main className="min-h-screen">{children}</main>
-      <footer>Footer</footer>
+      <Footer />
     </div>
   );
 }
